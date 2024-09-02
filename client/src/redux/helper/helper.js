cc9d37e33cd21931cf8b733e4faf22010de19f7a
abhisek123 <abhisek.rakshit@webskitters.com>
2024-09-02 17:09:31 +0530

    backend_url_updated

--- client/src/redux/helper/helper.js
+++ client/src/redux/helper/helper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { parseCookies } from 'nookies'
 
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://mern-ecommerce-api-0vbw.onrender.com/api/v1";
 
 export const axiosInstance = axios.create({
     baseURL:BASE_URL
